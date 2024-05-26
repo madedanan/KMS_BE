eb4b164379f3580193f0cd7fdfda9ed932d8b61e
madedanan <jayadanan82@gmail.com>
2024-05-26 10:03:44 +0700

    pre/posttes

---
+++ src/main/java/com/example/kms_poltekastra_be/rest/QuizRest.java
@@ -0,0 +1,26 @@
+package com.example.kms_poltekastra_be.rest;
+
+import com.example.kms_poltekastra_be.service.QuizService;
+import com.example.kms_poltekastra_be.service.impl.QuizServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/Quizs")
+public class QuizRest {
+    @Autowired
+    private QuizService quizService;
+
+    public QuizRest(QuizServiceImpl quizServiceImpl){
+        this.quizService = quizServiceImpl;
+    }
+
+    @PostMapping("/SaveDataQuiz")
+    public String saveDataQuiz(@RequestBody Map<String, Object> data) {
+        System.out.println("ADA" + data);
+        return quizService.saveDataQuiz(data);
+    }
+}
---
+++ src/main/java/com/example/kms_poltekastra_be/service/QuizService.java
@@ -0,0 +1,9 @@
+package com.example.kms_poltekastra_be.service;
+
+import java.util.Map;
+
+public interface QuizService {
+
+    public String getDataQuiz(Map<String, Object> data);
+    public String saveDataQuiz(Map<String, Object> data);
+}
